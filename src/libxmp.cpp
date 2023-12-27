0b0e73fe06ff97829a37da6b111c79edb3658088
MAIN\ayurkevich <ayurkevich@scnsoft.com>
2023-12-27 18:43:13 +0300

    init commit

---
+++ src/libxmp.cpp
@@ -0,0 +1,7 @@
+// see http://confluence:8090/display/DAK/Third+Party
+
+#include <string>
+#define XMP_INCLUDE_XMPFILES 1 //if using XMPFiles
+#define TXMP_STRING_TYPE std::string
+#include "XMP.hpp"
+#include "XMP.incl_cpp"
